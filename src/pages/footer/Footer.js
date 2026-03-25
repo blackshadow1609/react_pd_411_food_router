d5446bb3a069a3ebb78bfde2330ca218ce414604
Blackshadow <blackshadow0916@yandex.ru>
2026-03-26 00:55:40 +0300

    Font DONE

--- src/pages/footer/Footer.js
+++ src/pages/footer/Footer.js
@@ -2,9 +2,7 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div>
-
-        </div>
+        <div className='footer'>Copyright &copy; &ndash; {new Date().getFullYear()}</div>
     )
 }
 export default Footer;
